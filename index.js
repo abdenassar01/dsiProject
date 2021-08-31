5459acdb0c4bdab00cfedd52d3797c8f9a7a953c
bilalbajou <bajoubilal34@gmail.com>
2021-08-31 18:08:52 +0100

    finish contact page

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+
 // $(".menu-toggle-btn").click(function(){
 //     $(this).toggleClass("fa-times");
 //     $(".navigation-menu").toggleClass("active");
 //   });
 
-  $( ".menu-toggle-btn" ).on( "click", function() {
-    $(this).toggleClass("fa-times");
-    $(".navigation-menu").toggleClass("active");
-  });
-  $( ".menu-toggle-btn" ).trigger( "click" );
+$( ".menu-toggle-btn" ).on( "click", function() {
+  $(this).toggleClass("fa-times");
+  $(".navigation-menu").toggleClass("active");
+});
+$( ".menu-toggle-btn" ).trigger( "click" );
+
+ 
